8976bf7c0c6e441ef74a31f5c9af89347d18157d
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-04-28 21:59:13 -0400

    make it better

--- config/env.js
+++ config/env.js
@@ -2,9 +2,6 @@ const env ={
     port: 4000,
     mongodb_con: 'mongodb+srv://userNodeCafe:sebaghost11@cluster0.cugwa.mongodb.net/ikedaharketweb',
     secretorprivatekey:'hfo3JFfjfdCJDSofdj303CJDf304',
-    claveGmail:'hfwynkkksxhpqjxw',
+    claveGmail:'vvfxkrdgvzyxjxmc',
 }
-// PORT=4000
-// MONGODB_CON=mongodb+srv://userNodeCafe:sebaghost11@cluster0.cugwa.mongodb.net/ikedaharketweb
-// SECRETORPRIVATEKEY=hfo3JFfjfdCJDSofdj303CJDf304
 module.exports = env
--- models/server.js
+++ models/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-// const env = require('../config/env')
+const env = require('../config/env')
 const { dbConnection } = require('../database/config');
 
 class Server{
     
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = env.port;
         this.paths = {
             auth:       '/api/auth',
             habilidades:'/api/habilidades',
@@ -31,7 +31,7 @@ class Server{
         this.app.use(this.paths.auth,       require('../routes/auth'));
         this.app.use(this.paths.habilidades,require('../routes/habilidades'));
         this.app.use(this.paths.imagenes,   require('../routes/imagenes'));
-        this.app.use(this.paths.contacto,   require('../routes/contacto'));
+        //this.app.use(this.paths.contacto,   require('../routes/contacto'));
         this.app.use(this.paths.paginas,    require('../routes/paginas'));
     }
     listen(){
