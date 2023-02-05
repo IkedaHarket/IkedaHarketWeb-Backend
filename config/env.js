d8aaf17e16b656c31b1b0bbd4ca33da4294413f9
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2023-02-05 18:13:46 -0300

    port cambiado

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
-const env ={
+const env = {
     port: 4000,
     mongodb_con: 'mongodb+srv://userNodeCafe:sebaghost11@cluster0.cugwa.mongodb.net/ikedaharketweb',
-    secretorprivatekey:'hfo3JFfjfdCJDSofdj303CJDf304',
-    claveGmail:'vvfxkrdgvzyxjxmc',
+    secretorprivatekey: 'hfo3JFfjfdCJDSofdj303CJDf304',
+    claveGmail: 'vvfxkrdgvzyxjxmc',
 }
 module.exports = env
--- models/server.js
+++ models/server.js
@@ -3,40 +3,40 @@ const cors = require('cors');
 const env = require('../config/env')
 const { dbConnection } = require('../database/config');
 
-class Server{
-    
-    constructor(){
+class Server {
+
+    constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = env.port;
         this.paths = {
-            auth:       '/api/auth',
-            habilidades:'/api/habilidades',
-            imagenes:   '/api/imagenes',
-            contacto:   '/api/contacto',
-            paginas:    '/api/paginas'
+            auth: '/api/auth',
+            habilidades: '/api/habilidades',
+            imagenes: '/api/imagenes',
+            contacto: '/api/contacto',
+            paginas: '/api/paginas'
         }
         this.conectarDB();
         this.middlewares();
         this.routes();
     }
-    async conectarDB (){
+    async conectarDB() {
         await dbConnection();
     }
-    middlewares(){
+    middlewares() {
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.static('public'))
     }
-    routes(){
-        this.app.use(this.paths.auth,       require('../routes/auth'));
-        this.app.use(this.paths.habilidades,require('../routes/habilidades'));
-        this.app.use(this.paths.imagenes,   require('../routes/imagenes'));
+    routes() {
+        this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.habilidades, require('../routes/habilidades'));
+        this.app.use(this.paths.imagenes, require('../routes/imagenes'));
         //this.app.use(this.paths.contacto,   require('../routes/contacto'));
-        this.app.use(this.paths.paginas,    require('../routes/paginas'));
+        this.app.use(this.paths.paginas, require('../routes/paginas'));
     }
-    listen(){
-        this.app.listen(this.port,()=>{
-            console.log('Servidor corriendo en puerto: '+this.port);
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto: ' + this.port);
         })
     }
 }
